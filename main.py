ac93cd4b358bbdc735bc398cfdc4f003446248a6
roaan19 <141211644+roaan19@users.noreply.github.com>
2025-03-20 15:35:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+a=7
+print("type of a ", type(a))
+
+d=10.7
+print("type of d" , type(d))
+
+c="python"
+print("type of c" , type(c))
+
+e=False
+print("type of e" , type(e))
+
+#type casting 
+print("after type casting ")
+a=str(a)
+print("type of a ", type(a))
+d=int(d)
+print("type of d" , type(d))
+
+
+#reverse a string 
+g=input("enter the string")
+g=g[::-1]
+print("reverse of the string is ")
+print(g)
